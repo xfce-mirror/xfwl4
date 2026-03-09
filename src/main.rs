4ddeaa6a855589d23876677b84c765338b86d689
Brian J. Tarricone <brian@tarricone.org>
2026-03-08 22:48:58 -0700

    Rearrange modules/files in binary part of the crate

--- src/main.rs
+++ src/main.rs
@@ -14,31 +14,6 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
-//
-// Portions of this file are based on "anvil", an example compositor
-// based on the smithay crate, and are licensed under the MIT license
-// with the following terms:
-//
-// Copyright (C) Victor Berger <victor.berger@m4x.org>
-// Copyright (C) Drakulix (Victoria Brekenfeld)
-//
-// Permission is hereby granted, free of charge, to any person obtaining a
-// copy of this software and associated documentation files (the "Software"),
-// to deal in the Software without restriction, including without limitation
-// the rights to use, copy, modify, merge, publish, distribute, sublicense,
-// and/or sell copies of the Software, and to permit persons to whom the
-// Software is furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-// DEALINGS IN THE SOFTWARE.
 
 use std::time::Duration;
 
@@ -57,10 +32,12 @@ use xfwl4::{
     ui::{FromUiMessage, IconSizeHints, ToUiMessage},
 };
 
-use crate::cli::ChosenBackend;
+use crate::app::{
+    cli::{self, ChosenBackend},
+    env,
+};
 
-mod cli;
-mod env;
+mod app;
 
 #[cfg(feature = "profile-with-tracy-mem")]
 #[global_allocator]
