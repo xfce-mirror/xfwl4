c622add37776844ba1532bced50084da28b0c36f
Brian J. Tarricone <brian@tarricone.org>
2026-04-06 10:50:56 -0700

    Fix tabwin selection bg not showing on some themes

--- src/ui/tabwin.rs
+++ src/ui/tabwin.rs
@@ -785,11 +785,11 @@ mod imp {
                 if update_labels {
                     prev_widget_data.label.set_label("");
                 }
-                prev_widget_data.button_box.unset_state_flags(gtk::StateFlags::CHECKED);
+                prev_widget_data.window_button.unset_state_flags(gtk::StateFlags::CHECKED);
             }
 
             if let Some(widget_data) = self.client_widgets.borrow().get(&selected) {
-                widget_data.button_box.set_state_flags(gtk::StateFlags::CHECKED, false);
+                widget_data.window_button.set_state_flags(gtk::StateFlags::CHECKED, false);
                 if update_labels {
                     widget_data.label.set_label(widget_data.app_name.as_deref().unwrap_or("..."));
                     self.label.borrow().set_label(&widget_data.title);
