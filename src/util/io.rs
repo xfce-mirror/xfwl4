caa8d581b4d748f82731d4058b11e4aaf8f5e67f
Brian J. Tarricone <brian@tarricone.org>
2026-04-26 22:05:46 -0700

    Avoid using errno values not available on some OSes

--- src/util/io.rs
+++ src/util/io.rs
@@ -19,6 +19,16 @@ use std::os::fd::{AsFd, RawFd};
 
 use rustix::io::Errno;
 
+#[derive(Debug, Clone, thiserror::Error)]
+pub enum IoError {
+    #[error("not enough data or expected data not found")]
+    DataNotFound,
+    #[error("ran out of buffer space while reading")]
+    BufferTooSmall,
+    #[error("os error: {0}")]
+    Errno(#[from] Errno),
+}
+
 /// Closes all file descriptors, except stdin, stdout, stderr, and those in `except`.
 pub fn close_all_fds(except: &[RawFd]) {
     let mut keep = vec![libc::STDIN_FILENO, libc::STDOUT_FILENO, libc::STDERR_FILENO];
@@ -39,15 +49,15 @@ pub fn close_all_fds(except: &[RawFd]) {
 ///
 /// Note that, to ensure it doesn't read past `ends_with`, this reads only a single byte at a time,
 /// so it is inefficient if you need to write a lot of data before getting to `ends_with`.
-pub fn read_until<FD: AsFd>(fd: FD, buf: &mut [u8], ends_with: &[u8]) -> Result<usize, Errno> {
+pub fn read_until<FD: AsFd>(fd: FD, buf: &mut [u8], ends_with: &[u8]) -> Result<usize, IoError> {
     let mut total_len = 0;
     loop {
         match rustix::io::read(&fd, &mut buf[total_len..(total_len + 1)]) {
             Err(errno) if errno == Errno::INTR || errno == Errno::AGAIN => (),
-            Err(errno) => break Err(errno),
+            Err(errno) => break Err(errno.into()),
             Ok(len) if buf[0..(total_len + len)].ends_with(ends_with) => break Ok(total_len + len),
-            Ok(0) => break Err(Errno::NODATA),
-            Ok(len) if total_len + len == buf.len() => break Err(Errno::NOSPC),
+            Ok(0) => break Err(IoError::DataNotFound),
+            Ok(len) if total_len + len == buf.len() => break Err(IoError::BufferTooSmall),
             Ok(len) => total_len += len,
         }
     }
@@ -56,13 +66,13 @@ pub fn read_until<FD: AsFd>(fd: FD, buf: &mut [u8], ends_with: &[u8]) -> Result<
 /// Reads exactly the number of bytes available for storage in `buf`.
 ///
 /// Blocks until enough bytes are read, or until there is a read error or EOF.
-pub fn read_exact<FD: AsFd>(fd: FD, buf: &mut [u8]) -> Result<usize, Errno> {
+pub fn read_exact<FD: AsFd>(fd: FD, buf: &mut [u8]) -> Result<usize, IoError> {
     let mut total_len = 0;
     loop {
         match rustix::io::read(&fd, &mut buf[total_len..]) {
             Err(errno) if errno == Errno::INTR || errno == Errno::AGAIN => (),
-            Err(errno) => break Err(errno),
-            Ok(len) if len == 0 && total_len < buf.len() => break Err(Errno::NODATA),
+            Err(errno) => break Err(errno.into()),
+            Ok(len) if len == 0 && total_len < buf.len() => break Err(IoError::DataNotFound),
             Ok(len) if total_len + len == buf.len() => break Ok(total_len + len),
             Ok(len) => total_len += len,
         }
